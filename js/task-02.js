ce574cc5d2dea31f1fbdd08d685dee9a43e61425
Agnieszka Iwan <agnieszka.iwan2@gmail.com>
2023-03-27 22:35:28 +0200

    .

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+let li = document.createElement("li");
+
+ingredients.forEach(function (el) {
+  li = document.createElement("li");
+  li.textContent = el;
+  li.classList.add("item");
+  list.append(li);
+});
+console.log(li);
