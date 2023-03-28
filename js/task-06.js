875bd4648499c7bb187e671577df1b288f7df688
Agnieszka Iwan <agnieszka.iwan2@gmail.com>
2023-03-29 00:28:07 +0200

    task6

--- js/task-06.js
+++ js/task-06.js
@@ -4,7 +4,7 @@ const inputLength = inputText.getAttribute("data-length");
 inputText.addEventListener("blur", (event) => {
   let number = event.currentTarget.value.length;
 
-  if (number >= inputLength) {
+  if (number == inputLength) {
     inputText.classList.remove("invalid");
     inputText.classList.add("valid");
   } else {
