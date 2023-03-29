57f65e76cf3aeb1bb7efaa60ad7d2c32bcf23720
Agnieszka Iwan <agnieszka.iwan2@gmail.com>
2023-03-29 19:39:22 +0200

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,13 @@
+const btn = document.querySelector(".change-color");
+const hexColor = document.querySelector(".color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+btn.addEventListener("click", () => {
+  hexColor.innerHTML = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
+});
