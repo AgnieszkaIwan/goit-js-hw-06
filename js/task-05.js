65c789432251a431adb82602caf8eff47eb2a5d6
Agnieszka Iwan <agnieszka.iwan2@gmail.com>
2023-03-30 21:15:29 +0200

    task5

--- js/task-05.js
+++ js/task-05.js
@@ -4,3 +4,7 @@ const nameOutput = document.getElementById("name-output");
 nameInput.addEventListener("input", (event) => {
   nameOutput.textContent = event.currentTarget.value;
 });
+
+nameInput.addEventListener("input", (event) => {
+  nameOutput.textContent = nameInput.value ? nameInput.value : "Anonymous";
+});
